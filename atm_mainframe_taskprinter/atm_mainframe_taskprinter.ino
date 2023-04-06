2654ae8e03b629cf3c67ce398a626e92a8ab6384
fearmansighah <66765258+fearmansighah@users.noreply.github.com>
2023-04-05 21:09:26 -0700

    Update atm_mainframe_taskprinter.ino

--- atm_mainframe_taskprinter/atm_mainframe_taskprinter.ino
+++ atm_mainframe_taskprinter/atm_mainframe_taskprinter.ino
@@ -39,9 +39,9 @@ void setup()
   lcd.print("$");
   lcd.print(balance);
 
-  xTaskCreate(printSerial, "serial monitor printer", 100, NULL, 2, NULL);
-  xTaskCreate(pollButton1, "button1 polling", 100, NULL, 1, NULL);
-  xTaskCreate(pollButton2, "button2 pollig", 100, NULL, 1, NULL);
+  xTaskCreate(printSerial, "mainframe infoview", 100, NULL, 2, NULL);
+  xTaskCreate(pollButton1, "button1 poller", 100, NULL, 1, NULL);
+  xTaskCreate(pollButton2, "button2 poller", 100, NULL, 1, NULL);
   vTaskStartScheduler();
 }
 
